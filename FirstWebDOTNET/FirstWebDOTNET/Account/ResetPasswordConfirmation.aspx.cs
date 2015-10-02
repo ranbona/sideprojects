9bf80ced2c3ccd2015f2045edfbd88fcabc9d45b
Rafael Bonametti <rafabonametti@gmail.com>
2015-10-02 09:27:04 -0300

    Primeiro commit do site em asp.net

---
+++ FirstWebDOTNET/FirstWebDOTNET/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace FirstWebDOTNET.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ FirstWebDOTNET/FirstWebDOTNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstWebDOTNET.Global" Language="C#" %>
